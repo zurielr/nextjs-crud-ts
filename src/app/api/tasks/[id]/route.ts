4638992934ec0ed2df09ff80de441bef243ea38d
Zuriel Rubio <62489763+zurielr@users.noreply.github.com>
2023-12-27 02:36:03 +0000

    minute 29:48

---
+++ src/app/api/tasks/[id]/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/app/libs/prisma";
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    const task = await prisma.task.findFirst({
+        where: {
+            id: Number(params.id)
+        },
+    });
+    return NextResponse.json(task);
+}
+
+export async function PUT(request: Request, { params }: { params: { id: string } }) {
+    const data = await request.json();
+    const taskUpdated = await prisma.task.update({
+        where: {
+            id: Number(params.id),
+        },
+        data: data,
+    });
+    return NextResponse.json(taskUpdated);
+}
+
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+    const task = await prisma.task.delete({
+        where: {
+            id: Number(params.id)
+        },
+    });
+    return NextResponse.json(task);
+}
---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/app/libs/prisma";
+
+export async function GET() {
+    const tasks = await prisma.task.findMany()
+    return NextResponse.json(tasks);
+}
+
+export async function POST(request: Request) {
+    const data = await request.json();
+    const newTask = await prisma.task.create({
+        data
+    });
+    return NextResponse.json(newTask);
+}
